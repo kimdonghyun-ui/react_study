edd56673cdf303a365ad59aa5b08cdd7634fa44a
dongdong <bbodaekdh@gmail.com>
2021-02-28 13:49:27 +0900

    LNB작업중

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,25 +1,28 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Fab from "@material-ui/core/Fab";
-import Box from "@material-ui/core/Box";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import MailOutlineIcon from "@material-ui/icons/MailOutline";
-import logo from "../../images/common/logo.png";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Fab from '@material-ui/core/Fab';
+
+import MailOutlineIcon from '@material-ui/icons/MailOutline';
+import logo from '../../images/common/logo.png';
+import Lnblist from './Lnblist';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   wrap: {
-    display: "flex",
-    justifyContent: "space-between",
-    backgroundColor: "#d13030",
+    display: 'flex',
+    justifyContent: 'space-between',
+    backgroundColor: '#d13030',
   },
-  menuButton: {
-    //marginRight: theme.spacing(2),
+  fabButton: {
+    marginLeft: '12px',
+    width: '35px',
+    height: '35px',
+    backgroundColor: '#fff',
+    color: '#d13030',
   },
   title: {
     flexGrow: 1,
@@ -38,14 +41,7 @@ function Header() {
 
           <img src={logo} alt="" width="140" />
 
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
+          <Lnblist />
         </Toolbar>
       </AppBar>
     </div>
