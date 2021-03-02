512cbcdebf575ea11c6615a291635b77d5447939
김동현 <kimdonghyun@gimdonghyeon-ui-iMac.local>
2021-03-02 15:06:24 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ import Foodmenu from './containers/Foodmenu/Foodmenu';
 // import { Link } from 'react-router-dom';
 function App() {
   return (
-    <BrowserRouter basename="/react_study">
+    <HashRouter>
       <Switch>
         <Route exact path={['/', '/react_study']} component={Main} />
         <Route path="/page1" component={Page1} />
         <Route path="/page2" component={Page2} />
         <Route path="/menu" component={Foodmenu} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- src/components/common/lnb/Lnbitem.js
+++ src/components/common/lnb/Lnbitem.js
@@ -66,7 +66,7 @@ export default function SimpleAccordion() {
               - 메인으로 가기
             </Link>
             <Link
-              href="/page1"
+              to="/page1"
               component={Button}
               className={classes.sub_item}
               variant="body1"
