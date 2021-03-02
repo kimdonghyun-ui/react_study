4d11372ea56fd6428a13014e83fa7ff070e3bb63
김동현 <kimdonghyun@gimdonghyeon-ui-iMac.local>
2021-03-02 14:39:26 +0900

    수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { HashRouter, Route, Switch, BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter, Route, Switch, BrowserRouter , Router } from 'react-router-dom';
 import Main from './components/main';
 import Page1 from './components/page1';
 import Page2 from './components/page2';
@@ -8,34 +8,14 @@ import Foodmenu from './containers/Foodmenu/Foodmenu';
 // import { Link } from 'react-router-dom';
 function App() {
   return (
-    <HashRouter>
-      {/* <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <header>
-        <Link to="/">
-          <button>Main</button>
-        </Link>
-        <Link to="/page1">
-          <button>Page1</button>
-        </Link>
-        <Link to="/page2">
-          <button>Page2</button>
-        </Link>
-        <Link to="/menu">
-          <button>menu</button>
-        </Link>
-      </header> */}
+    <BrowserRouter basename="/">
       <Switch>
         <Route exact path={['/', '/react_study']} component={Main} />
         <Route path="/page1" component={Page1} />
         <Route path="/page2" component={Page2} />
         <Route path="/menu" component={Foodmenu} />
       </Switch>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
